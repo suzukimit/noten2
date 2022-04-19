c8142ce24049bc843e9e922e1b3b1ad5616f5fff
Mitsuru Suzuki <mtr.suzuki@gmail.com>
2022-04-20 00:41:45 +0900

    angularのバージョンアップ（v11.0へ）

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -34,9 +34,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      appRoutes,
-    )
+    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })
   ],
   exports: [
     RouterModule
