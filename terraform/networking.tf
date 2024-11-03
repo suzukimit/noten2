94892a18cc7258851daaefb2cc18bfd5d75b6217
suzukimit <mtr.suzuki@gmail.com>
2024-11-04 08:33:30 +0900

    コスト削減のためにLB関連のリソースを全削除

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,15 +1,15 @@
-# バケットのバックエンド設定
-resource "google_compute_backend_bucket" "frontend_deploy_bucket" {
-  name        = "frontend-backend-bucket"
-  bucket_name = google_storage_bucket.frontend_deploy.name
-  enable_cdn  = true
-  cdn_policy {
-    cache_mode = "FORCE_CACHE_ALL"
-  }
-}
-
-# URLマップの設定
-resource "google_compute_url_map" "cdn_url_map" {
-  name            = "frontend-url-map"
-  default_service = google_compute_backend_bucket.frontend_deploy_bucket.id
-}
+# # バケットのバックエンド設定
+# resource "google_compute_backend_bucket" "frontend_deploy_bucket" {
+#   name        = "frontend-backend-bucket"
+#   bucket_name = google_storage_bucket.frontend_deploy.name
+#   enable_cdn  = true
+#   cdn_policy {
+#     cache_mode = "FORCE_CACHE_ALL"
+#   }
+# }
+#
+# # URLマップの設定
+# resource "google_compute_url_map" "cdn_url_map" {
+#   name            = "frontend-url-map"
+#   default_service = google_compute_backend_bucket.frontend_deploy_bucket.id
+# }
--- terraform/ssl.tf
+++ terraform/ssl.tf
@@ -1,19 +1,19 @@
-resource "google_compute_managed_ssl_certificate" "managed_ssl_cert" {
-  name = "frontend-managed-ssl-cert"
-  managed {
-    domains = ["noten.tokyo"]
-  }
-}
-
-resource "google_compute_target_https_proxy" "https_proxy" {
-  name    = "frontend-https-proxy"
-  ssl_certificates = [google_compute_managed_ssl_certificate.managed_ssl_cert.id]
-  url_map = google_compute_url_map.cdn_url_map.id
-}
-
-resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
-  name                  = "frontend-https-forwarding-rule"
-  target                = google_compute_target_https_proxy.https_proxy.id
-  port_range            = "443"
-  load_balancing_scheme = "EXTERNAL"
-}
+# resource "google_compute_managed_ssl_certificate" "managed_ssl_cert" {
+#   name = "frontend-managed-ssl-cert"
+#   managed {
+#     domains = ["noten.tokyo"]
+#   }
+# }
+#
+# resource "google_compute_target_https_proxy" "https_proxy" {
+#   name    = "frontend-https-proxy"
+#   ssl_certificates = [google_compute_managed_ssl_certificate.managed_ssl_cert.id]
+#   url_map = google_compute_url_map.cdn_url_map.id
+# }
+#
+# resource "google_compute_global_forwarding_rule" "https_forwarding_rule" {
+#   name                  = "frontend-https-forwarding-rule"
+#   target                = google_compute_target_https_proxy.https_proxy.id
+#   port_range            = "443"
+#   load_balancing_scheme = "EXTERNAL"
+# }
